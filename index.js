f4cdefaa60de44dc3d04cf7a7a401a35eb03849c
Karan Kapoor <kk8658141@gmail.com>
2022-08-09 12:14:14 +0530

    error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const user = require("./Routes/user");
 const movie = require("./Routes/movie");
 const dbconnect = require("./Services/mongoose");
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 dbconnect();
 
@@ -13,8 +13,12 @@ app.use(express.json({limit : "50mb"}));
 app.use(express.urlencoded({limit : "50mb" , extended : false}))
 app.use(logger("dev"));
 app.use(cors());
-app.use("/api",user);
-app.use("/movie",movie);
+// app.use("/api",user);
+// app.use("/movie",movie);
+app.get("/",function(req,res)
+{
+    return res.send("Karan");
+})
 
 
 
