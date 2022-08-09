1510c80b7682bc4080a9d2b3dde981af60d761ec
Karan Kapoor <kk8658141@gmail.com>
2022-08-09 12:28:28 +0530

    error

--- index.js
+++ index.js
@@ -7,21 +7,27 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-dbconnect();
+// dbconnect();
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: false }));
+app.use(express.json({limit : "50mb"}));
+app.use(express.urlencoded({limit : "50mb" , extended : false}))
 app.use(logger("dev"));
 app.use(cors());
 // app.use("/api",user);
 // app.use("/movie",movie);
-app.get("/", function (req, res) {
-  return res.send("Karan");
-});
+app.get("/",function(req,res)
+{
+    return res.send("Karan");
+})
 
-app.listen(PORT, (error) => {
-  if (error) {
-    console.log(`Server Start fail ${error}`);
-  }
-  console.log(`Server start successfully port is ${PORT}`);
-});
+
+
+app.listen(PORT , (error) =>
+{
+    if(error)
+    {
+        console.log(`Server Start fail ${error}`);
+    }
+    
+        console.log(`Server start successfully port is ${PORT}`);
+})
